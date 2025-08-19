0bfdf2ae34dfdf83757aaf0642612281fb27d45b
CarlosJohncentVentura <johncent.developer@gmail.com>
2025-08-19 11:49:23 +0800

    tutorial-21-navigating-around

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,11 +1,19 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 import { globalStyles } from "../styles/global";
 
-export default function ReviewDetails() {
+//We got access from {navigation} props because this component is included
+//in a screen by createStackNavigator, which is coded in homeStack.js
+export default function ReviewDetails({ navigation }) {
+  const pressHandler = () => {
+    navigation.navigate("Home");
+    //navigation.push("Home");
+  };
+
   return (
     <View style={globalStyles.container}>
       <Text>Review Details Screen</Text>
+      <Button title="Back to Home Screen" onPress={pressHandler} />
     </View>
   );
 }
